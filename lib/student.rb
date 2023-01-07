b0770b2cd1faa0ee36d7fa738cc58f86540a5d25
Samuel Amity <sam.amity@gmail.com>
2023-01-07 12:41:41 -0500

    commit tree 2d1b97dc3ac560f84718d0d75992cf4d984927b1

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge.to_s
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
